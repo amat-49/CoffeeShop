42adfca78197cc8e0eace55d98c202b302e53626
Amat Addoais <amataddoais@gmail.com>
2026-03-18 04:38:39 -0400

    Changed the color of the tile to white for better user experience

--- CoffeeShop/CoffeeTileView.swift
+++ CoffeeShop/CoffeeTileView.swift
@@ -27,7 +27,7 @@ struct CoffeeTileView: View {
         }
         .frame(width:200, height:250)
         .background(RoundedRectangle(cornerRadius: 30))
-        .foregroundStyle(.green)
+        .foregroundStyle(.white)
         .shadow(radius: 5)
         .padding()
     
